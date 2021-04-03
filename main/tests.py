a3f8fb229e52b1ab10b78459ad3c986243cb1a1d
abdul muis <muiiizabdul@gmail.com>
2021-04-03 13:09:05 +0700

    add Dockerfile and docker-compose.yml and update .circleci/config.yml

--- main/tests.py
+++ main/tests.py
@@ -1,24 +1,18 @@
 from django.test import TestCase
 from django.conf import settings
-import requests
+from main.models.product import Shops
 
 # Create your tests here.
 class AddCustomerTesting(TestCase):
     def test_add_customer(self):
-        data = {
-            "name":"Abdul Muis",
-            "email":"muis@gmail.com",
-            "password": "testing",
-            "phone": "0881111",
-            "provinsi": "Jawa Barat",
-            "kabupaten": "Bogor",
-            "kecamatan": "Cigudeg",
-            "kode_pos": "16660"
-        }
-        url = '{}/api/main/customers/'.format(settings.MAIN_HOST)
-        response = requests.post(url, data = data)
-        print(response.content)
-        self.assertEqual(response.status_code, 200)
-        response2 = requests.post(url, data = data)
-        print(response.content)
-        self.assertEqual(response2.status_code, 400)
+        saved = Shops.objects.create(
+                customer_id=1,
+                name="Testing Shop",
+                bio="my testing shop",
+                url="/testing-shop"
+        )
+        self.assertEqual(saved.customer_id, 1)
+        self.assertEqual(saved.name, "Testing Shop")
+        self.assertEqual(saved.bio, "my testing shop")
+        self.assertEqual(saved.url, "/testing-shop")
+        saved.delete()
